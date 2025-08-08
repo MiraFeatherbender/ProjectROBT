8de99a69548df97d7180b74449b27ace65a51a25
MiraFeatherbender <jonathangorum813@gmail.com>
2025-08-07 21:50:30 -0500

    using build folder in repo root

---
+++ build/sketch/HallSensorHandler.cpp
@@ -0,0 +1,31 @@
+#line 1 "C:\\Users\\jonat\\OneDrive\\Old Documents\\GitHub\\ProjectROBT\\ROBT_Leg_Controller\\HallSensorHandler.cpp"
+#include "HallSensorHandler.h"
+#include "soc/gpio_struct.h"
+
+static HallSensorHandler* active_handler = nullptr;
+
+HallSensorHandler::HallSensorHandler()
+    : prev_state_(0), last_trigger_us_(0) {}
+
+void IRAM_ATTR HallSensorHandler::isrTrampoline() {
+    if (active_handler) active_handler->handleISR();
+}
+
+void IRAM_ATTR HallSensorHandler::handleISR() {
+    uint32_t now_us = micros();
+    if ((now_us - last_trigger_us_) < 3000) return; // Debounce
+
+    uint16_t gpioState = GPIO.in.val;
+    uint8_t curr_state = (gpioState >> PIN_HALL_NORTH) & 0x03;
+    if (curr_state == prev_state_) return;
+
+    prev_state_ = curr_state;
+    last_trigger_us_ = now_us;
+}
+
+bool HallSensorHandler::begin() {
+    active_handler = this;
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_NORTH), isrTrampoline, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_HALL_SOUTH), isrTrampoline, CHANGE);
+    return true;
+}
---
+++ build/sketch/SerialInputHandler.h
@@ -0,0 +1,26 @@
+#line 1 "C:\\Users\\jonat\\OneDrive\\Old Documents\\GitHub\\ProjectROBT\\ROBT_Leg_Controller\\SerialInputHandler.h"
+#ifndef SERIALINPUTHANDLER_H
+#define SERIALINPUTHANDLER_H
+#include <Arduino.h>
+#include <functional>
+#include <string>
+
+struct CommandSourceContext {
+    enum SourceType { serial_, esp_now_ } source;
+    std::function<void(const String&)> respond;
+};
+
+// Callback type for passing received lines and context to the parser
+using SerialLineCallback = std::function<void(const String&, const CommandSourceContext&)>;
+
+class SerialInputHandler {
+public:
+    SerialInputHandler();
+    void begin(unsigned long baud = 115200);
+    void update();
+    void setLineCallback(SerialLineCallback cb);
+private:
+    String buffer;
+    SerialLineCallback lineCallback;
+};
+#endif // SERIALINPUTHANDLER_H
